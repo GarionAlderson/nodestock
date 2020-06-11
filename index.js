a1a0ed3d347e6b2e4cddec9c800042ad70b2c9b8
GarionAlderson <garrygooch@aol.com>
2020-06-11 11:47:20 +0100

    Moved on to next bit

--- index.js
+++ index.js
@@ -3,8 +3,23 @@
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
+const request = require('request');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
+
+// API KEY - pk_a2adab08a5e54d369cbfd38e8a623f17
+// Creta API call function
+
+function call_api(finishedAPI){
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_a2adab08a5e54d369cbfd38e8a623f17', { json: true }, (err, res, body) => {
+	if (err) { return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI (body);
+	};
+});
+}
 
 // Set handlebars middleware
 app.engine('handlebars', exphbs());
@@ -12,9 +27,17 @@ app.set('view engine', 'handlebars');
 
 //Set handlebar routes
 app.get('/', function (req, res) {
-    res.render('home',{
-    	stuff:"This is poo Stuff..."
-    });
+	call_api(function(doneAPI){
+		res.render('home',{
+    	stock: doneAPI
+     });
+	});
+});
+
+//Create about page
+
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // Set static folder

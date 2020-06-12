fc6ffb0190c9447aeb46f52311e66fa0a0526ac9
GarionAlderson <garrygooch@aol.com>
2020-06-12 08:22:57 +0100

    added heroku toolbelt

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const request = require('request');
+const bodyParser = require('body-parser');
+
 const path = require('path');
 const PORT = process.env.PORT || 5000;
+
+//use middlewar bodyParser
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 
 // API KEY - pk_a2adab08a5e54d369cbfd38e8a623f17
 // Creta API call function
 
-function call_api(finishedAPI){
-request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_a2adab08a5e54d369cbfd38e8a623f17', { json: true }, (err, res, body) => {
+function call_api(finishedAPI, ticker){
+request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_a2adab08a5e54d369cbfd38e8a623f17', { json: true }, (err, res, body) => {
 	if (err) { return console.log(err);}
 	if (res.statusCode === 200){
 		//console.log(body);
@@ -25,13 +31,24 @@ request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_a2adab08a5e54d
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-//Set handlebar routes
+//Set handlebar index GET route
 app.get('/', function (req, res) {
 	call_api(function(doneAPI){
 		res.render('home',{
     	stock: doneAPI
      });
-	});
+	}, 'fb');
+});
+
+
+//Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI){
+		//posted_stuff = req.body.stock_ticker;
+		res.render('home',{
+    	stock: doneAPI,
+        });
+	}, req.body.stock_ticker );
 });
 
 //Create about page
